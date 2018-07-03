68ef7ae0ccc2a9e5748541402d62d513e08b18d2
jonroger3@gmail.com <jonroger3@gmail.com>
2018-07-03 20:33:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,14 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  const two = 2;
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
+ 
 }
+
+add2(46);
 
 var funkyFunction = function() {
   return function() {
